d24c7ca3ce5825a6ab7887beeb0b0dba7f76acbd
Natalie-Dor <varnata000@gmail.com>
2025-03-16 21:09:13 +0200

    swiper init scss for pages

---
+++ src/js/reviews.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let swiper = new Swiper('.mySwiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+});
