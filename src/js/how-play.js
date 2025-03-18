373e55c1d97360d8f7833f44703cd5a62af45a90
Natalie-Dor <varnata000@gmail.com>
2025-03-18 20:27:47 +0200

    section how to play

--- src/js/how-play.js
+++ src/js/how-play.js
@@ -1,1 +1,56 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
 
+document.addEventListener('DOMContentLoaded', function () {
+  let swiperInstance = null;
+
+  function initSwiper() {
+    if (window.innerWidth < 1200) {
+      if (!swiperInstance) {
+        swiperInstance = new Swiper('.swiper', {
+          slidesPerView: 1,
+          loop: true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return `
+                <svg class="${className}" width="8" height="8">
+                  <use href="./img/sprite.svg#${
+                    index === this.realIndex ? 'icon-dote' : 'icon-border-dote'
+                  }"></use>
+                </svg>`;
+            },
+          },
+          on: {
+            slideChange: function () {
+              document
+                .querySelectorAll('.swiper-pagination svg use')
+                .forEach((useEl, index) => {
+                  useEl.setAttribute(
+                    'href',
+                    `./img/sprite.svg#${
+                      index === this.realIndex
+                        ? 'icon-dote'
+                        : 'icon-border-dote'
+                    }`
+                  );
+                });
+            },
+          },
+        });
+      }
+    } else {
+      if (swiperInstance) {
+        swiperInstance.destroy(true, true);
+        swiperInstance = null;
+      }
+    }
+  }
+
+  // Викликаємо при завантаженні
+  initSwiper();
+
+  // Відстежуємо зміну розміру екрана
+  window.addEventListener('resize', initSwiper);
+});
