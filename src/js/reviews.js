865c056d0703555701702206f2e6038432003c2d
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-03-18 12:44:30 +0200

    remove

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let swiper = new Swiper('.mySwiper', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
-});
+
